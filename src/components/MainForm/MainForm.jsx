586ed4c8db0281dde52ceb30607f5aef4ede8f20
Maxim Molochkov <klaseca@gmail.com>
2019-06-05 00:12:57 +0400

    Added Marker Modal component

--- src/components/MainForm/MainForm.jsx
+++ src/components/MainForm/MainForm.jsx
@@ -1,22 +1,29 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 import AddTodo from "../AddTodo";
 import TodoList from "../TodoList";
+import MarkersModal from "../MarkersModal";
 
 export default class MainForm extends Component {
 	render() {
 		return (
-			<div className="main-form">
-				<div className="header">Todo React</div>
-				<TodoList />
-				<AddTodo />
-				<div className="trash-can">
-					<Link to="trashcan">
-						<img alt="trash" src="https://img.icons8.com/clouds/90/000000/trash.png" />
-					</Link>
+			<Fragment>
+				<div className="main-form">
+					<div className="header">Todo React</div>
+					<TodoList />
+					<AddTodo />
+					<div className="trash-can">
+						<Link to="trashcan">
+							<img
+								alt="trash"
+								src="https://img.icons8.com/clouds/90/000000/trash.png"
+							/>
+						</Link>
+					</div>
 				</div>
-			</div>
+				<MarkersModal />
+			</Fragment>
 		);
 	}
 }
